1e0b5ce37ffa13549128c85124eb939c47430779
Giorgio Alefe <giorgioa2002@gmail.com>
2021-09-18 17:51:40 -0300

    Create user profile (menu drop down)

---
+++ src/components/user-profile.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="user-profile">
+        <ul>
+            <li><a href="#">strGiorgio <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+            </svg></span></a>
+            <ul>
+                <li><a href="#">Config</a></li>
+                <li><a href="#">Change Avatar</a></li>
+                <hr>
+                <li><a href="#">Logout</a></li>
+            </ul>
+            </li>
+        </ul>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'userProfile'    
+}
+</script>
+
+<style scoped>
+    .user-profile ul{
+        background-color: var(--color-primary);
+        border: 2px solid var(--color-black-default);
+        font: 1.3rem var(--font-primary);
+        margin: 0;
+        padding: 10px;
+        position: relative;
+        list-style-type: none;
+    }
+
+    .user-profile ul li {
+        text-align: left;
+        display: inline-block;
+    }
+
+    .user-profile ul li a{
+        color: var(--color-black-default);
+        text-decoration: none;
+    }
+    
+    .user-profile ul li a span svg {
+        transition: .5s;
+    }
+
+    .user-profile ul:hover li a span svg {
+        transform: rotate(180deg);
+    }
+
+    .user-profile ul ul {
+        display: none;
+        position: absolute;
+        top: 100%;
+        right: 0;
+        z-index: 10;
+    }
+
+    .user-profile ul ul li{
+        width: 100%;
+        padding: 5px 0;
+    }
+
+    .user-profile ul ul li:hover {
+        background-color: var(--color-primary-light);
+    }
+
+    .user-profile ul:hover li ul {
+        display: block;
+    }
+    
+</style>
