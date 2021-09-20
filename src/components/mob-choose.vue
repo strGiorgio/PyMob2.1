8b0975619ea9c20caa967a825571ec9762cf694c
Giorgio Alefe <giorgioa2002@gmail.com>
2021-09-20 18:31:42 -0300

    Fix css measures

--- src/components/mob-choose.vue
+++ src/components/mob-choose.vue
@@ -1,5 +1,11 @@
 <template>
-    <div></div>
+    <div class="container">
+        <img src="/img/unknown.png" alt="Your mob apperence">
+        <div class="wrapper-buttons">
+            <button>Previous</button>
+            <button>Next</button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +15,32 @@ export default {
 </script>
 
 <style scoped>
+    .container {
+        border: 2px solid var(--color-black-default);
+        padding: 0;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        width: 30%;
+        height: 100%;
+    }
+
+    .container img {
+        width: 100%;
+        height: 90%;
+    }
+
+    .container .wrapper-buttons {
+        width: 100%;
+        height: 10%;
+    }
+
+    .container .wrapper-buttons button {
+        width: 50%;
+        height: 100%;
+        border: 2px solid var(--color-black-default);
+        font: 1rem var(--font-primary);
+        background-color: var(--color-secondary);
+    }
 
 </style>
