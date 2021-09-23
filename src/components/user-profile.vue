ce98a99dd6fe64d7c8293507815a7f0c0fb869d2
Giorgio Alefe <giorgioa2002@gmail.com>
2021-09-23 15:58:49 -0300

    Create a script to get name of logged user and put in user-profile

--- src/components/user-profile.vue
+++ src/components/user-profile.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="user-profile">
         <ul>
-            <li><a href="#">strGiorgio <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+            <li><a href="#">{{ user }} <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
             <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
             </svg></span></a>
             <ul>
@@ -18,7 +18,24 @@
 
 <script>
 export default {
-    name: 'userProfile'    
+    name: 'userProfile',
+    data() {
+        return {
+            user: null
+        }
+    },
+    methods: {
+        async getUser() {
+            const url = 'http://localhost:3000/logado';
+            const req = await fetch(url)
+            const res = await req.json();
+            this.user = res[0].name
+        }
+    },
+    mounted() {
+        this.getUser()
+    }
+
 }
 </script>
 
