ed65c15d132c0d8447ef130459996c294080bfbe
Giorgio Alefe <giorgioa2002@gmail.com>
2021-09-24 18:29:05 -0300

    Create a component to show attributes of the mob

---
+++ src/components/mob-attributes.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>Level</h3>
+        <h3>HP</h3>
+        <h3>Defense</h3>
+        <h3>Stamina</h3>
+        <h3>Strenght</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: 'mobAttributes'
+}
+</script>
