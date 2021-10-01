2b5048ca131dd166e08803900f62816998b51928
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-01 17:09:15 -0300

    Create help-box component

---
+++ src/components/help-box.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'helpBox'
+}
+</script>
+
+<style scoped>
+    .container {
+        border: 2px solid var(--color-black-default);
+        width: 60%;
+        height: 60%;
+    }
+</style>
