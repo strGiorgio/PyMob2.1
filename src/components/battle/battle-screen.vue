d07504d3ceef70c21330e485c340977b70e02732
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-05 17:53:15 -0300

    Create a new folder to put all battle components files and create battle-screen

---
+++ src/components/battle/battle-screen.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container"></div>
+</template>
+
+<script>
+export default {
+    name: 'battleScreen'
+}
+</script>
+
+<style scoped>
+    .container {
+        width: 100vw;
+        max-width: 1920px;
+        min-height: calc(100vh - 70px);
+        background-color: var(--color-primary);
+    }
+</style>
