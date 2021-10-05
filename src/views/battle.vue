d84ec8ed8f70d2eace23fb3674973a38fbd58856
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-05 17:51:35 -0300

    Create a new folder in views

---
+++ src/views/battle.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <battleScreen />
+    </div>
+</template>
+<script>
+import battleScreen from '../components/battle/battle-screen.vue'
+export default {
+    name: 'battle',
+    components: {
+        battleScreen
+    }
+}
+</script>
