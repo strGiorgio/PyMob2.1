b4445fe149d7323494d913f2d12bf70e64eb8a51
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-06 17:19:04 -0300

    Create controller component

---
+++ src/components/battle/controller.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="controller">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'controller'
+}
+</script>
+
+<style scoped>
+    .controller {
+        width: 300px;
+        height: 160px;
+        background-color: var(--color-secondary);
+        border: 2px solid var(--color-black-default);
+    }
+</style>
