ea8238d8dc3f05815068b815d38487309297a9e9
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-06 17:48:05 -0300

    Create a div area to wrapper battle components

--- src/components/battle/battle-screen.vue
+++ src/components/battle/battle-screen.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="container">
-        <controller />
+    <div class="battle-arena">
+        <h1>Welcome to Arena</h1>
+        <div class="area">
+            <controller />
+        </div>
     </div>
 </template>
 
@@ -15,10 +18,30 @@ export default {
 </script>
 
 <style scoped>
-    .container {
+    .battle-arena {
         width: 100vw;
         max-width: 1920px;
         min-height: calc(100vh - 70px);
         background-color: var(--color-primary);
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 30px 0 0 0;
+        margin: 0 auto;
+    }
+
+    .battle-arena h1 {
+        font: 2.3rem var(--font-primary);
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+    .area {
+        width: 80%;
+        height: 550px;
+        background-color: #fff;
+        position: relative;
+        overflow: hidden;
     }
 </style>
