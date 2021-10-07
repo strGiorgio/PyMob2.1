5fd01ec9f89032bf0f4dd8b268ff435ea064bda8
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-07 18:14:13 -0300

    Create new items and style controller with grid and buttons

--- src/components/battle/controller.vue
+++ src/components/battle/controller.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="controller">
+        <div class="wrapper-img">
+            <img src="/img/avatar.png" alt="">
+        </div>
+        <div class="controller-buttons">
+            <progress max="100" value="10" ></progress>
+            <button>Atack</button>
+            <button>Atack Especial</button>
+            <button>Escape</button>
+            <button>Regenerate</button>
+        </div>
         
     </div>
 </template>
@@ -12,7 +22,7 @@ export default {
 
 <style scoped>
     .controller {
-        width: 300px;
+        width: 450px;
         height: 160px;
         background-color: var(--color-secondary);
         border: 2px solid var(--color-black-default);
@@ -21,6 +31,37 @@ export default {
         right: 200px;
         transform: translateX(450px);
         animation: slideToLeft 2s forwards;
+        padding: 5px;
+        display: grid;
+        grid-template-columns: 1.3fr 2fr;
+        
+    }
+
+    .wrapper-img img{
+        width: 90%;
+        height: 100%;
+    }
+
+    .controller-buttons {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr 1fr fr;
+        grid-gap: 5px;
+    }
+    
+    .controller-buttons progress {
+        grid-column: 1 / 3;
+    }
+
+    .controller-buttons button {
+        background-color: var(--color-gray);
+        border: 2px solid var(--color-black-default);
+        font: 1rem var(--font-primary);
+        transition: .5s;
+    }
+
+    .controller-buttons button:hover {
+        opacity: 70%;
     }
 
     @keyframes slideToLeft {
