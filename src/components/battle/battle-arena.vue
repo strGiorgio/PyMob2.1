a8868b934f65633ae7d61ecafbc612ff05cb47d1
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-09 17:47:13 -0300

    Creata arena to put mobs, enemies, floor and background

---
+++ src/components/battle/battle-arena.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="battle-arena">
+        <img class="floor-arena" src="/img/arena.png" alt="Floor arena">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'battleArena'
+}
+</script>
+
+<style scoped>
+    .battle-arena {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+    }
+</style>
