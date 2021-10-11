07ff448af8dcff4adf5437cf839a21d789123811
Giorgio Alefe <giorgioa2002@gmail.com>
2021-10-11 17:31:26 -0300

    Create mob and enemy position in battle-arena

--- src/components/battle/battle-arena.vue
+++ src/components/battle/battle-arena.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="battle-arena">
-        <img class="floor-arena" src="/img/arena.png" alt="Floor arena">
+        <div class="wrapper-floor-mobs">
+            <img class="floor-arena" src="/img/arena.png" alt="Floor arena">
+
+            <!-- Mobs -->
+            <img class="mob-image" src="/img/tartaruga-idle.png" alt="">
+
+            <!-- Enemies -->
+            <img class="enemy-image" src="/img/morcego.png" alt="">
+        </div>
     </div>
 </template>
 
@@ -16,5 +24,28 @@ export default {
         height: 100%;
         display: flex;
         justify-content: center;
+        align-items: center;
+    }
+
+    .battle-arena .wrapper-floor-mobs {
+        position: relative;
+        width: 512px;
+        height: 256px;
+    }
+
+    .battle-arena .mob-image {
+        width: 128px;
+        height: 200px;
+        position: absolute;
+        bottom: 50px;
+        left: 50px;
+    }
+
+    .battle-arena .enemy-image {
+        width: 128px;
+        height: 180px;
+        position: absolute;
+        top: -80px;
+        right: 50px;
     }
 </style>
