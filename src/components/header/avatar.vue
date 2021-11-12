3aaeb534fe1194abe6203b9588b1261fd6589efe
Giorgio Alefe <giorgioa2002@gmail.com>
2021-11-12 14:36:43 -0300

    Create a async function to get user avatar and put in your screen

--- src/components/header/avatar.vue
+++ src/components/header/avatar.vue
@@ -1,16 +1,42 @@
 <template>
     <div class="moldure">
-        <img class="img-avatar" :src="img" :alt="description">
+        
     </div>
 </template>
+
 <script>
 export default {
     name: 'avatar',
     data() {
         return {
-            img: '/img/avatar.png',
+            avatar: null,
             description: 'Ursinho Balaclava'
         }
+    },
+    methods: {
+        async getAvatar() {
+            const dbLogado = await fetch('http://localhost:3000/logado');
+            const logado = await dbLogado.json();
+
+            const dbUsers = await fetch('http://localhost:3000/users');
+            const users = await dbUsers.json()
+
+            for (var i in users) {
+                if (logado[0].name == users[i].name && logado[0].passwd == users[i].passwd) {
+                    //Pega o avatar no banco de dados e muda avatar do usuário na tela
+                    const moldure = document.querySelector('.moldure');
+                    this.avatar = users[i].avatar
+                    console.log(`Your avatar: ${this.avatar}`)
+                    moldure.style.backgroundImage = `url(${this.avatar})`;
+                    break
+                }
+            }
+
+            
+        }
+    },
+    mounted() {
+        this.getAvatar()
     }
 }
 </script>
@@ -22,6 +48,8 @@ export default {
         border: 2px solid var(--color-black-default);
         overflow: hidden;
         box-sizing: border-box;
+        background-image: url('');
+        background-size: cover ;
     }
 
     .img-avatar {
