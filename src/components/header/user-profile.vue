654f7cea9ea0a6d6a4a9e3dc7f310a3f9eaef00c
Giorgio Alefe <giorgioa2002@gmail.com>
2021-11-13 13:23:06 -0300

    Import change-avatar in user-profile

--- src/components/header/user-profile.vue
+++ src/components/header/user-profile.vue
@@ -1,28 +1,35 @@
 <template>
-    <div class="user-profile">
-        <ul>
-            <li><a href="#">{{ user }} <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-            </svg></span></a>
+    <div>
+        <div class="user-profile">
             <ul>
-                <li><a href="#">Config</a></li>
-                <li><a href="#">Change Avatar</a></li>
-                <hr>
-                <li><a href="#" @click="logout">Logout</a></li>
+                <li><a href="#">{{ user }} <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                </svg></span></a>
+                <ul>
+                    <li><a href="#">Config</a></li>
+                    <li><a href="#">Change Avatar</a></li>
+                    <hr>
+                    <li><a href="#" @click="logout">Logout</a></li>
+                </ul>
+                </li>
             </ul>
-            </li>
-        </ul>
-        
+        </div>
+        <changeAvatar />
     </div>
 </template>
 
 <script>
+import changeAvatar from './change-avatar.vue'
+
 export default {
     name: 'userProfile',
     data() {
         return {
             user: null
         }
+    },
+    components: {
+        changeAvatar
     },
     methods: {
         async getUser() {
