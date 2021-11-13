27cd19403aee0a46c0579f625cb4f295463bf936
Giorgio Alefe <giorgioa2002@gmail.com>
2021-11-13 13:49:18 -0300

    Create a emit when close-button is clicked

--- src/components/header/change-avatar.vue
+++ src/components/header/change-avatar.vue
@@ -1,6 +1,6 @@
 <template>
     <form class="change-avatar">
-        <div class="close-button">
+        <div class="close-button" @click="$emit('closed')">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" class="bi bi-x" viewBox="0 0 16 16">
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
             </svg>
@@ -12,7 +12,8 @@
 
 <script>
 export default {
-    name: 'changeAvatar'    
+    name: 'changeAvatar',
+    emits: ['closed']
 }
 </script>
 
@@ -55,5 +56,10 @@ export default {
         background-color: var(--color-secondary);
         padding: 5px 0;
         border: 1.5px solid var(--color-black-default);
+        transition: .3s;
+    }
+
+    .change-avatar input[type="submit"]:hover {
+        transform: scale(1.03);
     }
 </style>
