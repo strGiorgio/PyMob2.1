0f6bece5082e9005f55948c3ce36447924dec11f
Giorgio Alefe <giorgioa2002@gmail.com>
2021-11-15 15:04:05 -0300

    Create a function when submited, and add a placeholder in input

--- src/components/header/change-avatar.vue
+++ src/components/header/change-avatar.vue
@@ -1,11 +1,11 @@
 <template>
-    <form class="change-avatar">
+    <form class="change-avatar" @submit="sendToDB($event)">
         <div class="close-button" @click="$emit('closed')">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" class="bi bi-x" viewBox="0 0 16 16">
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
             </svg>
         </div>
-        <input type="url" required min="2">
+        <input type="url" required min="2" v-model="url" placeholder="Ex: https://i.imgur.com/perros">
         <input type="submit" value="change avatar">
     </form>
 </template>
@@ -13,7 +13,18 @@
 <script>
 export default {
     name: 'changeAvatar',
-    emits: ['closed']
+    data() {
+        return {
+            url: null
+        }
+    },
+    emits: ['closed'],
+    methods: {
+        sendToDB(e) {
+            e.preventDefault()
+            console.log(this.url)
+        }
+    }
 }
 </script>
 
